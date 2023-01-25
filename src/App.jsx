c37fa91499f0210fba78a8c35360d7a1c08f05f7
Gustavo <ingsuarezgustavo@gmail.com>
2023-01-25 17:49:21 -0400

    a2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import "./index.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-import MainLayout from "./pages/MainLayout";
 import ProtectedRoutes from "./pages/ProtectedRoutes";
 import PokemonDetail from "./Pages/PokemonDetail";
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <div className="App">
-        {/* Aquello que queremos en TODAS las vistas */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard/:name" element={<PokemonDetail/>} />
@@ -18,7 +16,7 @@ function App() {
           </Route>
         </Routes>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
